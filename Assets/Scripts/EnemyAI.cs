479b01e16964ccdb49f8442f00f4dfd4c026e577
niucongcong <mayukirinn@gmail.com>
2017-02-12 01:04:44 +0800

    basic logic

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,20 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyAI : MonoBehaviour
 {
     public EnemyInfo Info;//各项数据
     public float ForceDestroyTime = 10;//强制销毁时间
     private float generateTime = 0;//生成时间
+    public Text HpText;//显示血量
 
     /// <summary>
     /// 初始化数据
     /// </summary>
     /// <param name="boss">是否是Boss</param>
-    /// <param name="diff">当前难度等级</param>
-    public void Init(bool boss, int diff)
+    public void Init(bool boss)
     {
+        int diff = GameController.Instance.totalBossCount;
         generateTime = Time.time;
         int maxHp = 0;
         int speed = 0;
@@ -29,9 +31,10 @@ public class EnemyAI : MonoBehaviour
             min = min > 5 ? 5 : min;
             int max = diff > 6 ? 6 : diff;
             maxHp = Random.Range(min, max);
-            speed = 100 + (int)(diff*2.5f);
+            speed = 100 + (int)(diff*5f);
         }
         Info = new EnemyInfo(boss, maxHp, speed);
+        EventTriggerListener.Get(gameObject).onClick = OnHit;
     }
 
     void Update()
@@ -48,6 +51,7 @@ public class EnemyAI : MonoBehaviour
         if (Info == null)
             return;
         transform.Translate(0, -Info.Speed*Time.deltaTime, 0);
+        HpText.text = Info.CurHp.ToString();
     }
 
     /// <summary>
@@ -57,5 +61,42 @@ public class EnemyAI : MonoBehaviour
     {
         if (Time.time - generateTime >= ForceDestroyTime)
             Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// 进入触发器
+    /// </summary>
+    void OnTriggerEnter(Collider co)
+    {
+        switch (co.tag)
+        {
+            case "Base":
+                GameController.Instance.OnHit(Info.Boss ? 5 : 1);
+                CheckEnd();
+                break;
+            case "Shield":
+                Info.Speed = (int)(Info.Speed*(1 - GameController.Instance.ShieldReduce));
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 点击以消灭此星星
+    /// </summary>
+    public void OnHit(GameObject go)
+    {
+        Info.CurHp -= GameController.Instance.Damage;
+        if (Info.CurHp <= 0)
+            CheckEnd();
+    }
+
+    /// <summary>
+    /// 检查销毁自己时是否触发特效
+    /// </summary>
+    void CheckEnd()
+    {
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public enum GameStatus
@@ -27,8 +28,24 @@ public class EnemyInfo
     }
 }
 
-public class tempGameController : MonoBehaviour
+public class GameController : MonoBehaviour
 {
+    private static GameController instance;
+
+    public static GameController Instance
+    {
+        get { return instance; }
+        set { instance = value; }
+    }
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(gameObject);
+        else
+            Instance = this;
+    }
+
     public GameObject StarPrefab;//普通怪预设
     public GameObject BossPrefab;//BOSS预设
     public Transform EnemyParent;//敌人的父物体
@@ -38,9 +55,30 @@ public class tempGameController : MonoBehaviour
     public Text[] ShieldText;//护盾说明文字,0为Shield,1为效能说明
     private int MaxBaseHp = 100;//基地最大血量
     private int CurBaseHp = 0;//基地当前血量
-    private float ShieldReduce = 0.8f;//护盾减速效能
+    public float ShieldReduce = 0.6f;//护盾减速效能
+    public int Damage = 1;//单次点击伤害量
 
     public GameStatus mStatus = GameStatus.Idle;//游戏状态
+
+    void Start()
+    {
+        Init();
+    }
+
+    /// <summary>
+    /// 初始化数据
+    /// </summary>
+    void Init()
+    {
+        CurBaseHp = MaxBaseHp = 100;
+        ShieldReduce = 0.6f;
+        Damage = 1;
+        BaseHp.fillAmount = 1;
+        ShieldImg.CrossFadeAlpha(1, 0, false);
+        ShieldText[1].text = "Speed Reduce: 60%";
+        for (int i = 0; i < ShieldText.Length; i++)
+            ShieldText[i].CrossFadeAlpha(1, 0, false);
+    }
 
     void Update()
     {
@@ -61,7 +99,8 @@ public class tempGameController : MonoBehaviour
     public float GenerateIntervalTime = 0.3f;//生成间隔
     private float _lastTime = 0;//上次生成的时间点
     private int totalEnemyCount = 0;//当前总共生成的敌人数量
-    public int BossWave = 30;//当totalEnemyCount达到指定BossWave的倍数后生成Boss
+    public int totalBossCount = 0;//当前总共生成的boss数量，充当难度等级
+    public int BossWave = 10;//当totalEnemyCount达到指定BossWave的倍数后生成Boss，随波数递增
     void CheckGenerateLogic()
     {
         if(mStatus == GameStatus.Gaming)
@@ -90,12 +129,35 @@ public class tempGameController : MonoBehaviour
     void GenerateEnemy()
     {
         bool bBoss = totalEnemyCount > 0 && totalEnemyCount % BossWave == 0;//判断是否是Boss
-        int iDiff = totalEnemyCount/BossWave;//难度等级，每生成一个Boss增加1级
         GameObject enemy = Instantiate(bBoss ? BossPrefab : StarPrefab);//实例化敌人预设体
         enemy.transform.position = new Vector3(Random.Range(50, 851), 1650); //设定生成位置
         enemy.transform.parent = EnemyParent;//加入父物体
         EnemyAI ai = enemy.GetComponent<EnemyAI>();
-        ai.Init(bBoss, iDiff);
-        totalEnemyCount++;
+        ai.Init(bBoss);
+        if (bBoss)
+        {
+            totalBossCount++;
+            BossWave += 5;
+            totalEnemyCount = 0;
+        }
+        else
+            totalEnemyCount++;
+    }
+
+    /// <summary>
+    /// 基地被星星击中
+    /// </summary>
+    /// <param name="dmg">伤害量</param>
+    public void OnHit(int dmg)
+    {
+        CurBaseHp -= dmg;
+        float percentage = (float) CurBaseHp/MaxBaseHp;
+        BaseHp.fillAmount = percentage;
+        ShieldReduce = CurBaseHp*0.006f;
+        ShieldImg.CrossFadeAlpha(percentage, 0, false);
+        ShieldText[1].text = "Speed Reduce: " + (ShieldReduce*100).ToString("f0") + "%";
+        ShieldText[0].CrossFadeAlpha(percentage, 0, false);
+        //for (int i = 0; i < ShieldText.Length; i++)
+        //    ShieldText[i].CrossFadeAlpha(percentage, 0, false);
     }
 }
